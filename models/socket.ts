9b4c7a2cd6c0bda8cf5e5dd2687735125940adb7
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-09-16 20:47:55 -0600

    listen and emit to match event

--- models/socket.ts
+++ models/socket.ts
@@ -1,6 +1,6 @@
 import socketio from 'socket.io';
 import { validateJWTSocket } from '../helpers/validateJWTSocket';
-import { getUsers, saveMessage, userConnect } from '../controllers/socket';
+import { getMatchedUser, getUsers, saveMessage, userConnect } from '../controllers/socket';
 
 class Socket {
     private io: socketio.Server;
@@ -25,8 +25,7 @@ class Socket {
             // * marcar como conectado al usuario
             await userConnect(base_user!, true); 
             // console.log('usuario conectado: ', base_user)
-
-            
+         
             // * emitir los usuarios conectados
             this.io.emit('user-list', await getUsers())
             // console.log('emitiendo usuarios')
@@ -39,6 +38,14 @@ class Socket {
                 const newMessage = await saveMessage(data);
                 this.io.to(`room ${ newMessage.from }`).emit('new-message', newMessage);
                 this.io.to(`room ${ newMessage.to }`).emit('new-message', newMessage);
+            })
+
+            // * escuchar si hay un nuevo match
+            socket.on('new-match', async data => {
+                const from = await getMatchedUser(data.from);
+                const to = await getMatchedUser(data.to);
+                this.io.to(`room ${ from?.base_user }`).emit('new-match')
+                this.io.to(`room ${ to?.base_user }`).emit('new-match')
             })
 
             // * desconectar al usuario (online: false en el modelo)           
