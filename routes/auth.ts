c83c7049b06f4c7adb7b702b226e4c956f911a9b
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-10-06 21:26:04 -0600

    call to socket server endpoint to update user's name

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { signUpController } from '../controllers/auth';
+import { signUpController, updateUserController } from '../controllers/auth';
 import { validateJWT } from '../middlewares/validateJWT';
 
 const router = Router();
 
 router.post('/sign-up', validateJWT, signUpController);
+router.patch('/user', validateJWT, updateUserController);
 
 export default router;
