d3b137185767cb54ff97e96b56bd931ce46e0957
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-10-12 19:28:10 -0600

    dockerization for chat

--- database/connection.ts
+++ database/connection.ts
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 export const databaseConnect = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL!);
+        const db_url = !process.env.NODE_ENV
+                               ? process.env.DEV_DATABASE_URL
+                               : process.env.PROD_DATABASE_URL; 
+        await mongoose.connect(db_url!);
         console.log('Successful connection');
     } catch (error) {
         console.log(`Connection error: ${ error }`);
